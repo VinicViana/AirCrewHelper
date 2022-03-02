415de9a575957977a03060e6f87f84f264f61797
Vinicius Viana Oliveira Soares <vvosoares@hotmail.com>
2022-03-01 23:44:47 -0300

    AirCrewHelper - ManagerView criada

---
+++ AirCrewHelper_View/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace AirCrewHelper_View
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.manageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pilotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.airCrewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.airplaneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procedureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkListsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewLogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(680, 400);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageToolStripMenuItem,
+            this.logoutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1040, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // manageToolStripMenuItem
+            // 
+            this.manageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pilotToolStripMenuItem,
+            this.airCrewToolStripMenuItem,
+            this.airplaneToolStripMenuItem,
+            this.procedureToolStripMenuItem,
+            this.checkListsToolStripMenuItem,
+            this.viewLogsToolStripMenuItem});
+            this.manageToolStripMenuItem.Name = "manageToolStripMenuItem";
+            this.manageToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.manageToolStripMenuItem.Text = "Manage";
+            // 
+            // pilotToolStripMenuItem
+            // 
+            this.pilotToolStripMenuItem.Name = "pilotToolStripMenuItem";
+            this.pilotToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pilotToolStripMenuItem.Text = "Pilots";
+            // 
+            // airCrewToolStripMenuItem
+            // 
+            this.airCrewToolStripMenuItem.Name = "airCrewToolStripMenuItem";
+            this.airCrewToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.airCrewToolStripMenuItem.Text = "AirCrews";
+            // 
+            // airplaneToolStripMenuItem
+            // 
+            this.airplaneToolStripMenuItem.Name = "airplaneToolStripMenuItem";
+            this.airplaneToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.airplaneToolStripMenuItem.Text = "Airplanes";
+            // 
+            // procedureToolStripMenuItem
+            // 
+            this.procedureToolStripMenuItem.Name = "procedureToolStripMenuItem";
+            this.procedureToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.procedureToolStripMenuItem.Text = "Procedures";
+            this.procedureToolStripMenuItem.Click += new System.EventHandler(this.procedureToolStripMenuItem_Click);
+            // 
+            // checkListsToolStripMenuItem
+            // 
+            this.checkListsToolStripMenuItem.Name = "checkListsToolStripMenuItem";
+            this.checkListsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkListsToolStripMenuItem.Text = "CheckLists";
+            // 
+            // viewLogsToolStripMenuItem
+            // 
+            this.viewLogsToolStripMenuItem.Name = "viewLogsToolStripMenuItem";
+            this.viewLogsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewLogsToolStripMenuItem.Text = "View Logs";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            this.logoutToolStripMenuItem.Click += new System.EventHandler(this.logoutToolStripMenuItem_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 614);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmPrincipal";
+            this.Text = "AirCrewHelper";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem manageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pilotToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem airCrewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem airplaneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procedureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkListsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewLogsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+    }
+}
+
---
+++ AirCrewHelper_View/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AirCrewHelper_View
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void procedureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
